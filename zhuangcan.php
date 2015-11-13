32c0d6323bf59dd141e0fae7f35342c2dc0adb07
codergma <codergma@163.com>
2015-11-13 11:57:33 +0800

    正则

--- zhuangcan.php
+++ zhuangcan.php
@@ -1,24 +1,33 @@
 <?php
-// $pattern="/src=(\\)[\"][\s\S]*['\"]/iU";
-$pattern="/http[\s\S]*['\"]/iU";
- // $pattern="/src=(\\\\)['\"]/iU";
+// $pattern="/http[\s\S]*['\"]/iU";
+
+// $filename = "/home/liubin/zhuangcan.txt";
+// $subject = file_get_contents($filename);
+// $out_filename = "/home/liubin/out_put.txt";
+// if ($subject)
+// {
+// 	preg_match_all($pattern, $subject, $matches,PREG_PATTERN_ORDER);
+// 	file_put_contents($out_filename, $matches);
+// 	$res = array();
+// 	foreach ($matches[0] as $value)
+// 	{
+// 		array_push($res,substr($value,0,-2));
+// 	}
+// 	var_dump($res);
+// }
+// else{
+// 	exit('aaa');
+// }
 
 $filename = "/home/liubin/zhuangcan.txt";
-$subject = file_get_contents($filename);
-$out_filename = "/home/liubin/out_put.txt";
-if ($subject)
+$str = file_get_contents($filename);
+$pattern="/src=\\\\['\"]http[\s\S]*['\"]/iU";
+$pattern2="/http[\s\S]*['\"]/iU";
+preg_match_all($pattern, $str, $matches,PREG_PATTERN_ORDER);
+$res = array();
+foreach($matches[0] as $value)
 {
-	preg_match_all($pattern, $subject, $matches,PREG_PATTERN_ORDER);
-	file_put_contents($out_filename, $matches);
-	$res = array();
-	foreach ($matches[0] as $value)
-	{
-		array_push($res,substr($value,0,-2));
-	}
-	var_dump($res);
+	preg_match_all($pattern2, $value, $matches2,PREG_PATTERN_ORDER);
+	$res = array_merge($res,$matches2[0]);
 }
-else{
-	exit('aaa');
-}
-
-
+var_dump($res);
