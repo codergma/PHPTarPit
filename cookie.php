c4dc4a7c0cf2488a57f3e07d09268793aea932ac
codergma <codergma@gmail.com>
2015-11-25 17:56:51 +0800

    modified

--- cookie.php
+++ cookie.php
@@ -3,8 +3,10 @@ setcookie()发送一个cookie给客户端
 注意：
 setcookie()之前不能有任何实际的输出
 setcookie()可以删除cookie，设置过期时间为过去的时间点，
-有一个坑需要注意就是，删除的时候要设置path参数，否则会默认当前页面路径，
-如果删除路径与设置路径不一致，则删除不了。
+有两个坑需要注意
+(1)删除的时候要设置path参数，否则会默认当前页面路径，
+   如果删除路径与设置路径不一致，则删除不了。
+(2)有时候不设置path,domain，虽然setcookie返回true,但是不能在浏览器创建cookie;
 -->
 <?php
 //echo "不能有输出";
@@ -19,10 +21,10 @@ $domain = "";
 
 $result = setcookie($name,$value,$expire,$path,$domain);
 var_dump($result);
-echo $_COOKIE[$name];//setcookie()函数调用之后，并不能立即生效，所以并没有输出值
-										//所以最好在setcookie()函数之后接着执行 $_COOKIE[$name]=$value;
+//setcookie()函数调用之后，并不能立即生效，所以并没有输出值
+//所以最好在setcookie()函数之后接着执行 $_COOKIE[$name]=$value;
+echo $_COOKIE[$name];
 
-//还发现一个问题，有时候不设置path,domain，虽然setcookie返回true,但是不能在浏览器创建cookie;
 
 
 
--- header.php
+++ header.php
@@ -12,26 +12,33 @@ header()的使用有三点需要注意
 <?php
 //先看正确的用法
 //(1)HTTP/开头的报文，用来计算将要发送的HTTP状态码;
-header('HTTP/1.0 404 Not Found');
+ // header('HTTP/1.0 404 Not Found');
 //(2)Location:开头的报文，不仅把报文发送给浏览器，还返回给浏览器一个重定向的状态码302;
- header('Location:http://www.baidu.com');
+ // header('Location:http://www.baidu.com');
 
 //错误的用法
 //(1)
 //输出大量内容，导致服务器不能缓存,php配置中display_errors=On
 //页面报错　Warning:Cannot modify header information -headers already sent by ..... 
-
+// ob_start();
 for($i=0;$i<1e5;$i++)
 {
 	echo $i;
 }
 header("location:http://www.smaryun.com");
+// ob_end_flush();
 
 //(2)
 //刷新缓冲区，强制输出内容，同样报错，可以确认是服务器缓存没有输出的问题了
-echo 'abc';
-ob_flush();//刷新php的缓冲区
-flush();//刷新apache的缓冲区
-header('location:http://www.smaryun.com');
+// echo 'abc';
+// ob_flush();//刷新php的缓冲区
+// flush();//刷新apache的缓冲区
+// header('location:http://www.smaryun.com');
 
+//(3)
+//通过输出缓冲函数延迟脚本输出,知道设置好了cookie和　http头
+// ob_start();
+// ...
+// header();
+// ob_end_flush();
 
