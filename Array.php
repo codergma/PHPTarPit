88f87f8be8499be0c482f62f52f6a50d42d489e2
codergma <codergma@gmail.com>
2015-12-08 18:19:07 +0800

    array

--- Array.php
+++ Array.php
@@ -1,4 +1,9 @@
 <?php
+
+function walk_callback($value,$key)
+{
+	echo $value.'--'.$key.PHP_EOL;
+}
 $arr = array(
 	'username'=>'codergma',
 	'info_uptime'=>111111,
@@ -24,17 +29,24 @@ array_values(input);
 $vals = array_values($arr);
 echo implode($vals,',');
 echo '<br/>';
-// 合并数组,如果只给了一个数组(或者另一个是空)并且该数组是数字索引的，则键名会以连续方式重新索引
+// 合并数组,如果是数字索引，键名将会重新索引
 array_merge();
 $arr1 = array(2=>'x',3=>'y');
-$arr2 = array();
+$arr2 = array(5=>'z',8=>'w');
 $arr3 = array_merge($arr1,$arr2);
+var_dump($arr3);
+$arr3 = $arr1 + $arr2;
 var_dump($arr3);
 // 移除数组第一个元素 
 array_shift();
-//array_walk
-function walk_callback($value,$key)
-{
-	echo $value.'--'.$key.PHP_EOL;
-}
+// array_walk
 array_walk($arr, 'walk_callback');
+// array_slice默认会重新排序并重置数组的数字索引。
+// 你可以通过将 preserve_keys 设为 TRUE 来改变此行为。
+$slice = array_slice($arr, 0,2,TRUE);
+var_dump($slice);
+var_dump($arr);
+// 把数组中的一部分去掉并用其它值取代 
+$splice = array_splice($arr, 0,2);
+var_dump($splice);
+var_dump($arr);
